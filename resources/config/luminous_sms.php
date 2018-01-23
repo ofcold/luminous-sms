b13e8c60a374479b603debcc40eccbbc484d6abc
bill li <billli@billdeiMac.local>
2018-01-23 16:31:26 +0800

    Rename handler.

--- resources/config/luminous_sms.php
+++ resources/config/luminous_sms.php
@@ -2,19 +2,19 @@
 
 return [
 	/**
-	 *	The default send SMS gateway
+	 *	The default send SMS handler
 	 *
 	 *	Use multiple third party service providers. We need to use a main selection of services. It is used by default to
 	 *	push SMS messages.
 	 *
 	 *	@var		string
 	 */
-	'default_gateway'		=> 'qclod',
+	'default_handler'		=> 'qclod',
 
 	'supported'		=> [
 
 		/**
-		 *	Tencent qclod gateway handler.
+		 *	Tencent qclod handler.
 		 *
 		 *	@var		array
 		 */
