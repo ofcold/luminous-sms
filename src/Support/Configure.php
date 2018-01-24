50302b1f61b8e4a85a77fd3bfc303cf51e4ab680
bill li <billli@billdeiMac.local>
2018-01-24 08:58:58 +0800

    Modified Configure.

--- src/Support/Configure.php
+++ src/Support/Configure.php
@@ -43,7 +43,7 @@ class Configure
 	 */
 	public static function hasItem(string $key) : bool
 	{
-		return static::has(static::$items, $key);
+		return Arrays::has(static::$items, $key);
 	}
 
 	/**
@@ -61,7 +61,7 @@ class Configure
 			return $this->getMany($key);
 		}
 
-		return static::get(static::$items, $key, $default);
+		return Arrays::get(static::$items, $key, $default);
 	}
 
 	/**
@@ -82,7 +82,7 @@ class Configure
 				list($key, $default) = [$default, null];
 			}
 
-			$config[$key] = static::get(static::$items, $key, $default);
+			$config[$key] = Arrays::get(static::$items, $key, $default);
 		}
 
 		return $config;
@@ -102,168 +102,7 @@ class Configure
 
 		foreach ( $keys as $key => $value )
 		{
-			static::set(static::$items, $key, $value);
+			Arrays::set(static::$items, $key, $value);
 		}
-	}
-
-	/**
-	 *	Determine whether the given value is array accessible.
-	 *
-	 *	@param		mixed		$value
-	 *
-	 *	@return		bool
-	 */
-	public static function accessible($value) : bool
-	{
-		return is_array($value) || $value instanceof ArrayAccess;
-	}
-
-	/**
-	 *	Get an item from an array using "dot" notation.
-	 *
-	 *	@param		\ArrayAccess|array		$array
-	 *	@param		string					$key
-	 *	@param		mixed					$default
-	 *
-	 *	@return		mixed
-	 */
-	public static function get($array, $key, $default = null)
-	{
-		if ( ! static::accessible($array) )
-		{
-			return static::value($default);
-		}
-
-		if ( is_null($key) )
-		{
-			return $array;
-		}
-
-		if ( static::exists($array, $key) )
-		{
-			return $array[$key];
-		}
-
-		if ( strpos($key, '.') === false )
-		{
-			return $array[$key] ?? static::value($default);
-		}
-
-		foreach ( explode('.', $key) as $segment )
-		{
-			if ( static::accessible($array) && static::exists($array, $segment) )
-			{
-				$array = $array[$segment];
-			}
-			else
-			{
-				return static::value($default);
-			}
-		}
-
-		return $array;
-	}
-
-	/**
-	 *	Check if an item or items exist in an array using "dot" notation.
-	 *
-	 *	@param		\ArrayAccess|array		$array
-	 *	@param		string|array			$keys
-	 *
-	 *	@return		bool
-	 */
-	public static function has($array, $keys)
-	{
-		if ( is_null($keys) )
-		{
-			return false;
-		}
-
-		$keys = (array) $keys;
-
-		if ( ! $array )
-		{
-			return false;
-		}
-
-		if ( $keys === [] )
-		{
-			return false;
-		}
-
-		foreach ( $keys as $key )
-		{
-			$subKeyArray = $array;
-
-			if ( static::exists($array, $key) )
-			{
-				continue;
-			}
-
-			foreach ( explode('.', $key) as $segment )
-			{
-				if ( ! static::accessible($subKeyArray) && ! static::exists($subKeyArray, $segment) )
-				{
-					return false;
-				}
-
-				$subKeyArray = $subKeyArray[$segment];
-			}
-		}
-
-		return true;
-	}
-
-	/**
-	 *	Determine if the given key exists in the provided array.
-	 *
-	 *	@param		\ArrayAccess|array		$array
-	 *	@param		string|int				$key
-	 *
-	 *	@return		bool
-	 */
-	public static function exists($array, $key) : bool
-	{
-		if ( $array instanceof ArrayAccess )
-		{
-			return $array->offsetExists($key);
-		}
-
-		return array_key_exists($key, $array);
-	}
-
-	/**
-	 *	Return the default value of the given value.
-	 *
-	 *	@param		mixed		$value
-	 *
-	 *	@return		mixed
-	 */
-	public static function value($value)
-	{
-		return $value instanceof Closure ? $value() : $value;
-	}
-
-	/**
-	 *	Generate a more truly "random" alpha-numeric string.
-	 *
-	 *	@param		int		$length
-	 *
-	 *	@return		string
-	 */
-	public static function random(int $length = 16) : string
-	{
-		$string = '';
-
-		while ( ($len = strlen($string)) < $length )
-		{
-			$size = $length - $len;
-
-			$bytes = random_bytes($size);
-
-			$string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
-		}
-
-		return $string;
 	}
 }
