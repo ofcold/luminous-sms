9c75552c55f7b5ba6f32c3d8ed995a14c899e16e
Bill Li <bill.li@anomaly.ink>
2018-03-27 11:43:50 +0800

    Update HandlerBadException.php

--- src/Exceptions/HandlerBadException.php
+++ src/Exceptions/HandlerBadException.php
@@ -8,11 +8,13 @@ use Exception;
  *	Class HandlerBadException
  *
  *	@link			https://anomaly.ink
+ *
  *	@author			Anomaly lab, Inc <support@anomaly.ink>
  *	@author			Bill Li <bill@anomaly.ink>
+ *
  *	@package		AnomalyLab\LuminousSMS\Exceptions\HandlerBadException
  */
 class HandlerBadException extends Exception
 {
-
+  
 }
