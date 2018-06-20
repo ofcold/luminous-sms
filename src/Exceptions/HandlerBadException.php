2cecb195e3a5efcfc0a8b35d4bdad204bc75f16e
Bill li <bill.li@ofcold.com>
2018-06-20 11:13:38 +0800

    HandlerBadException.php

---
+++ src/Exceptions/HandlerBadException.php
@@ -0,0 +1,24 @@
+<?php
+
+use Ofcold\LuminousSMS\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Class HandlerBadException
+ *
+ * @link  https://ofcold.com
+ * @link  https://ofcold.com/license
+ *
+ * @author  Ofcold <support@ofcold.com>
+ * @author  Olivia Fu <olivia@ofcold.com>
+ * @author  Bill Li <bill.li@ofcold.com>
+ *
+ * @package  Ofcold\LuminousSMS\Exceptions\HandlerBadException
+ *
+ * @copyright  Copyright (c) 2017-2018, Ofcold. All rights reserved.
+ */
+class HandlerBadException extends RuntimeException
+{
+
+}
