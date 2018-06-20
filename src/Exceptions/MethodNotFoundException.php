6a6c93726f8b0707f66696d43d4f5a9d7c9ec475
Bill li <bill.li@ofcold.com>
2018-06-20 09:10:16 +0800

    MethodNotFoundException.php

---
+++ src/Exceptions/MethodNotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Class MethodNotFoundException
+ *
+ * @link  https://ofcold.com
+ * @link  https://ofcold.com/license
+ *
+ * @author  Ofcold <support@ofcold.com>
+ * @author  Olivia Fu <olivia@ofcold.com>
+ * @author  Bill Li <bill.li@ofcold.com>
+ *
+ * @package  Ofcold\LuminousSMS\Exceptions\MethodNotFoundException
+ *
+ * @copyright  Copyright (c) 2017-2018, Ofcold. All rights reserved.
+ */
+class MethodNotFoundException extends RuntimeException
+{
+
+}
