bda9ff367f6e4644637e595e56a5d0b4661d1dcc
Bill li <bill.li@ofcold.com>
2018-06-20 11:15:07 +0800

    nice Sender.php

--- src/Qcold/Sender.php
+++ src/Qcold/Sender.php
@@ -1,18 +1,81 @@
 <?php
 
-use Ofcold\LuminousSMS\Contracts\HandersInterface;
+use Ofcold\LuminousSMS\Handlers;
 
 class Sender
 {
+	/**
+	 * The qcloud instance.
+	 *
+	 * @var Qcloud
+	 */
 	protected $qcloud;
 
-	public function __construct(HandersInterface $qcloud)
+	/**
+	 * Create an a new Sender.
+	 *
+	 * @param Qcloud $qcloud
+	 */
+	public function __construct(Qcloud $qcloud)
 	{
 		$this->qcloud = $qcloud;
 	}
 
+	/**
+	 * Sender SMS
+	 *
+	 * @return mixed
+	 */
 	public function render()
 	{
+		$method = method_exists($this, $this->qcloud->getMessage()->getType())
+			 ? $this->qcloud->getMessage()->getType()
+			 : 'text';
+
+		$params = $this->$method();
+
+		//	Set SMS flag.
+		if ( $sign = $this->qcloud->getMessage()->getSign() )
+		{
+			$params['sign']	= $sign;
+		}
+
+		//	Set the full mobile phone.
+		$params['tel']	= [
+			'nationcode'	=> $this->qcloud->getMessage()->getCode(),
+			'mobile'		=> $this->qcloud->getMessage()->getMobilePhone()
+		];
+
+		$params['time'] = time();
+		$params['ext'] = '';
+
+		$random = Helpers::random(10);
+
+		$params['sig'] = GenerateSign::make($params, $randomm, $this->qcloud->getConfig('app_key'));
+
+		$result = $this->qcloud->request(
+			'post',
+			sprintf(
+				'%s%s?sdkappid=%s&random=%s',
+				Qcloud::REQUEST_URL,
+				Qcloud::REQUEST_METHOD[$this->qcloud->getType()],
+				$this->qcloud->getConfig('app_id'),
+				$random
+			),
+			[
+				'headers'	=> [
+					'Accept' => 'application/json'
+				],
+				'json'  => $params,
+			]
+		);
+
+		if ( 0 != $result['result'] )
+		{
+			throw new HandlerBadException($result['errmsg'], $result['result'], $result);
+		}
+
+		return $result;
 
 	}
 
@@ -21,15 +84,13 @@ class Sender
 	 *
 	 * Text SMS request body.
 	 *
-	 * @param  MessagerInterface  $messager
-	 *
 	 * @return  array
 	 */
-	protected function text(MessagerInterface $messager) : array
+	protected function text() : array
 	{
 		return [
-			'type'		=> (int)($messager->getType() !== 'text'),
-			'msg'		=> $messager->getContent(),
+			'type'		=> (int)($this->qcloud->getMessage()->getType() !== 'text'),
+			'msg'		=> $this->qcloud->getMessage()->getContent(),
 			'extend'	=> ''
 		];
 	}
@@ -39,14 +100,12 @@ class Sender
 	 *
 	 * Voice SMS request body.
 	 *
-	 * @param  MessagerInterface  $messager
-	 *
 	 * @return  array
 	 */
-	protected function voice(MessagerInterface $messager) : array
+	protected function voice() : array
 	{
 		return [
-			'promptfile'	=> $messager->getContent(),
+			'promptfile'	=> $this->qcloud->getMessage()->getContent(),
 			'prompttype'	=> 2,
 			'playtimes'		=> 2
 		];
